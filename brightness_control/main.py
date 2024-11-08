238c897d64ef1bc9bcad293ebf3181df72e7f88a
borislav dimitrov <borislav-dimitrov@outlook.com>
2024-11-08 20:02:51 +0200

    Handle unsupported monitor exception

--- brightness_control/main.py
+++ brightness_control/main.py
@@ -1,9 +1,13 @@
 from gui import App
-
+from screen_brightness_control.exceptions import ScreenBrightnessError
+from tkinter import messagebox
 
 def main():
-    app = App()
-    app.run()
+    try:
+        app = App()
+        app.run()
+    except ScreenBrightnessError as _:
+        messagebox.showerror(title='Error!', message='Unsupported monitor/s!')
 
 if __name__ == '__main__':
     main()
