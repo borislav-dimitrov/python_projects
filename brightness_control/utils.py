02a42736c1721acf25a7810281698fe7d31790b6
borislav dimitrov <borislav-dimitrov@outlook.com>
2024-11-08 17:21:31 +0200

    Adjust typing annotations

--- brightness_control/utils.py
+++ brightness_control/utils.py
@@ -1,8 +1,9 @@
 import os
 import sys
+from typing import LiteralString
 
 
-def resource_path(relative_path) -> str:
+def resource_path(relative_path) -> LiteralString | str | bytes:
     try:
         # sys._MEIPASS is the files directory when opened from pyinstaller exe
         base_path = sys._MEIPASS
