03172365be025a7b72ef7c40e91e9c41cb4964aa
borislav dimitrov <borislav-dimitrov@outlook.com>
2024-11-08 17:10:17 +0200

    Adjust the compile script

--- brightness_control/compile.bat
+++ brightness_control/compile.bat
@@ -1,1 +1,11 @@
-pyinstaller --onefile --distpath "..\..\..\compiled\brightness control" --icon="icon.ico" --name="Brightness Controller" "main.py"
+call ".\venv\Scripts\activate" && pyinstaller ^
+ --onefile ^
+ --distpath "..\..\..\compiled\brightness control" ^
+ --icon="icon.ico" ^
+ --noconsole ^
+ --add-data "icon.ico;." ^
+ --paths="venv\Lib\site-packages" ^
+ --name="Brightness Controller" ^
+ "main.py"
+
+del "Brightness Controller.spec"
