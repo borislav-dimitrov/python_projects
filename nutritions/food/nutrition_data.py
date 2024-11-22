26b1a6c1315e46563e0ccb5ee7ded2d615202b84
Borislav Dimitrov <76029499+borislav-dimitrov@users.noreply.github.com>
2024-11-22 11:06:57 +0200

    modify nutrition data file

---
+++ nutritions/food/nutrition_data.py
@@ -0,0 +1,68 @@
+NUTRITION_PER_100_GRAMS = {
+    "LIDL_FRESHONA_chickpeas_can": {
+        "calories": 127,
+        "protein": 6.7,
+        "fat": 2.7,
+        "carbs": 16.5
+    },
+    "LIDL_NIXE_tuna_fillets": {
+        "calories": 91,
+        "protein": 21.1,
+        "fat": 0.6,
+        "carbs": 0
+    },
+    "LIDL_CROWNFIELD_oat_flakes": {
+        "calories": 351,
+        "protein": 12,
+        "fat": 5.7,
+        "carbs": 62
+    },
+    "LIDL_VEMONDO_oat_milk": {
+        "calories": 37,
+        "protein": 0.4,
+        "fat": 1.2,
+        "carbs": 5.6
+    },
+    "LIDL_SNACKDAY_wholemeal_tortilla_wrap": {
+        "calories": 181,
+        "protein": 5.9,
+        "fat": 4.4,
+        "carbs": 27.7
+    },
+    "EGG": {
+        "calories": 78,
+        "protein": 7.5,
+        "fat": 5.4,
+        "carbs": 0
+    },
+    "banana": {
+        "calories": 110,
+        "protein": 1,
+        "fat": 0,
+        "carbs": 28
+    },
+    "OLYMPUS_izvara": {
+        "calories": 90,
+        "protein": 14,
+        "fat": 2,
+        "carbs": 4
+    },
+    "green_olives": {
+        "calories": 143,
+        "protein": 0.9,
+        "fat": 15,
+        "carbs": 0
+    },
+    "CROCO_crackers_mix": {
+        "calories": 476,
+        "protein": 8.8,
+        "fat": 19.9,
+        "carbs": 64
+    },
+    "baked_almonds": {
+        "calories": 629,
+        "protein": 25,
+        "fat": 54,
+        "carbs": 5.8
+    }
+}
